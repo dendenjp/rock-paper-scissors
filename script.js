fd7b8cf8b2fdda25728404036e1375200af971e0
dendenjp <daimlerjp@gmail.com>
2022-08-18 04:42:40 -0500

    Added UI.

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const allButtons = document.querySelectorAll('.btn');
 const btnRock = document.querySelector('.btnRock');
 const btnPaper = document.querySelector('.btnPaper');
 const btnScissors = document.querySelector('.btnScissors');
+const playerScoreEl = document.querySelector('.score-player');
+const computerScoreEl = document.querySelector('.score-computer');
 const score = document.querySelector('.score');
-const result = document.querySelector('.result');
+const resultSelection = document.querySelector('.result-selection');
+const resultGame = document.querySelector('.result-game');
+
+const resultContainer = document.querySelector('.result-container');
+
+let playerScore = 0;
+let computerScore = 0;
 
 btnRock.value = 'rock';
 btnPaper.value = 'paper';
@@ -19,26 +27,29 @@ function computerPlay() {
     } else return 'scissors';
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function playRound(playerSelection, computerSelection) {
-    let playerDraw = `You chose ${playerSelection}. Computer chose ${computerSelection}. The round is a draw!`;
-    let playerWin = `You chose ${playerSelection}. Computer chose ${computerSelection}. You win this round!`;
-    let playerLose = `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose this round!`;
+    let playerDraw = `You chose ${playerSelection}. Computer chose ${computerSelection}.`;
+    let playerWin = `You chose ${playerSelection}. Computer chose ${computerSelection}.`;
+    let playerLose = `You chose ${playerSelection}. Computer chose ${computerSelection}.`;
     // score.textContent = `Your score is ${playerScore} and computer is ${computerScore}`;
 
     if (playerSelection === computerSelection) {
-        result.textContent = playerDraw;
-        score.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        resultSelection.textContent = playerDraw;
+        resultGame.textContent = 'The game is a draw!';
+
+        // score.textContent = `You: ${playerScore} Computer: ${computerScore}`;
     } else if (
         (playerSelection === 'rock' && computerSelection === 'paper') ||
         (playerSelection === 'scissors' && computerSelection === 'rock') ||
         (playerSelection === 'paper' && computerSelection === 'scissors')
     ) {
         ++computerScore;
-        result.textContent = playerLose;
-        score.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        resultSelection.textContent = playerLose;
+        resultGame.textContent = 'You lose!';
+
+        // score.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        playerScoreEl.textContent = playerScore;
+        computerScoreEl.textContent = computerScore;
     }
 
     //add another else if for playerWin
@@ -48,15 +59,32 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'paper' && computerSelection === 'rock')
     ) {
         ++playerScore;
-        result.textContent = playerWin;
-        score.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        resultSelection.textContent = playerWin;
+        resultGame.textContent = 'You win!';
+
+        // score.textContent = `You: ${playerScore} Computer: ${computerScore}`;
+        playerScoreEl.textContent = playerScore;
+        computerScoreEl.textContent = computerScore;
     } //else is if input is not a rock, paper, or scissors
     else console.log('Invalid input. Please try again.');
+
+    if (playerScore === 5 || computerScore === 5) console.log('GAME ENDS!');
+}
+
+function resetPage() {
+    resultContainer.classList.add('--visible');
+    resultContainer.addEventListener('transitionend', function () {
+        resultContainer.classList.remove('--visible');
+
+        console.log('reset!');
+    });
 }
 
 allButtons.forEach((button) =>
     button.addEventListener('click', function () {
         playRound(button.value, computerPlay());
+        resultContainer.classList.remove('--hidden');
+        resetPage();
     })
 );
 
